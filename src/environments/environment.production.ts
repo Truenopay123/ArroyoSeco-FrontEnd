7348a4bac143144f16bcc27de82db0dfdf3c9f47
ManuelSMC-UTEQ <2022371045@uteq.edu.mx>
2026-03-24 12:28:18 -0600

    Corrigiendo error de MB máximo

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://arroyoseco-api-production.up.railway.app/api',
+  apiUrl: 'https://arroyoseco-api-production-fcae.up.railway.app/api',
   mercadoPagoPublicKey: 'TEST-c4e8fec7-4dc4-4c52-a300-5fa8385f85cf'
 };
