7f3387cdbd14196c79fdb302c40d34fac3daa970
ManuelSMC-UTEQ <2022371045@uteq.edu.mx>
2026-03-24 10:58:39 -0600

    Ajustando proyecto para production

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:7190/api',
+  apiUrl: 'https://arroyoseco-api-production.up.railway.app/api',
   mercadoPagoPublicKey: 'TEST-c4e8fec7-4dc4-4c52-a300-5fa8385f85cf'
 };
